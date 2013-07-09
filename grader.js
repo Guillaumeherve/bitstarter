1ba5adaa94705f5bcb61c2636c066ecd24fae356
Guillaume HERVE <guillaume.xd@gmail.com>
2013-07-09 06:45:40 +0000

    grader update

--- grader.js
+++ grader.js
@@ -24,10 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
+var rest = require('./restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URLFILE_DEFAULT ="http://google.com";
+var URLFILE ="http://google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -63,13 +63,12 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-
-
-if(require.main == module) {
+require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.option('-u, --url <url_file>', 'Path to a .html', clone(assertFileExists), URLFILE_DEFAULT)
+        .option('-f, --checks <file>', 'Path to required checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+.option('-f, --url [URL]', 'Path to optional URL', assertURLExists, URL_DEFAULT)
+
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
